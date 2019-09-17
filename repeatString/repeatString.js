7cdd5a55504bb98effb8c25a7c8e20f8cd791bbc
Joan Advincula <jadvincula.cloud@protonmail.com>
2019-09-17 18:06:20 +0800

    Write solutions for helloWorld, repeatString, reverseString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(userStr, numRepeats) {
+    let resultStr = "";
 
+    if (numRepeats < 0) {
+        return 'ERROR';
+    }
+    
+    while (numRepeats > 0) {
+        resultStr += userStr;
+        numRepeats--;
+    }
+
+    return resultStr;
 }
 
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(userStr) {
+    let userStrLen = userStr.length;
+    let resultStr = "";
 
+    for (let i = userStrLen - 1; i >= 0; i--) {
+        resultStr += userStr.charAt(i);
+    }
+
+    return resultStr;
 }
 
 module.exports = reverseString
