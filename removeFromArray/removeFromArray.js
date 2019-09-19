f6bdc2f72d1db871c8168a076fbe704c8fe64438
Joan Advincula <jadvincula.cloud@protonmail.com>
2019-09-19 18:37:18 +0800

    Solve removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,29 @@
-const removeFromArray = function() {
+// Needs to accept multiple optional arguments
+// Will not work if total items to remove exceeds four
+const removeFromArray = function(userArray, item0, item1, item2, item4) {
+    // Transform all arguments into an array
+    let userArgs = Array.from(arguments);
+    // Remove first argument, original array
+    userArgs.shift();
 
+    // Evaluate each item to remove
+    let finalArray = userArray;
+    for (let i = 0; i < userArgs.length; i++) {
+        if (typeof userArgs[i] === "undefined") {
+            continue;
+        }
+        
+        // Check for presence in array
+        if (finalArray.includes(userArgs[i])) {
+            let itemIndex = finalArray.indexOf(userArgs[i]);
+            let resultFront = finalArray.slice(0, itemIndex);
+            let resultBack = finalArray.slice(itemIndex + 1);
+
+            finalArray = resultFront.concat(resultBack);
+        }
+    }
+    
+    return finalArray;
 }
 
 module.exports = removeFromArray
