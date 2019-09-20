5905a3f7114cb81759e00855cfc582f5f2c4e3e2
Joan Advincula <jadvincula.cloud@protonmail.com>
2019-09-20 16:33:23 +0800

    Solve tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
-
+// Round to one decimal place
+const roundToOne = function(userNum) {
+  return Math.round(userNum * 10) / 10;
 }
 
-const ctof = function() {
+const ftoc = function(userTemp) {
+  let resultCTemp = (userTemp - 32) * (5 / 9);
+  return roundToOne(resultCTemp);
+}
 
+const ctof = function(userTemp) {
+  let resultFTemp = userTemp * (9 / 5) + 32;
+  return roundToOne(resultFTemp);
 }
 
 module.exports = {
