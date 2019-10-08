6ba8ac85eb9ec0367563c0a0edce2247821f7ef2
Joan Advincula <jadvincula.cloud@protonmail.com>
2019-10-08 17:30:15 +0800

    Write solution for calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,52 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (numList) {
+	if (numList.length == 0) {
+		return 0;
+	}
+
+	const numTotal = numList.reduce((total, x) => {
+		return total += x;
+	}, 0);
+
+	return numTotal;
 }
 
-function multiply () {
-	
+function multiply (numList) {
+	if (numList.length == 0) {
+		return 0;
+	}
+
+	const numTotal = numList.reduce((total, x) => {
+		return total *= x;
+	}, 1);
+
+	return numTotal;	
 }
 
-function power() {
-	
+function power(a, b) {
+	return Math.pow(a, b);
 }
 
-function factorial() {
-	
+function factorial(x) {
+	let fact = 1;
+	let currentNum = 1;
+	if (x == 0) {
+		return 1;
+	} else {
+		while (currentNum <= x) {
+			fact *= currentNum;
+			currentNum += 1;
+		}
+	}
+
+	return fact;
 }
 
 module.exports = {
