ab1442157b3b5e755a18fb635c3cb2e9d3e8f89e
Joan Advincula <jadvincula.cloud@protonmail.com>
2019-10-11 16:50:05 +0800

    Solve caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,7 +1,7 @@
 const caesar = function(plaintext, shift) {
     /**
      * ASCII reference
-     * upper case alphabet: 65 to 907
+     * upper case alphabet: 65 to 90
      * lower case alphabet: 97 to 122
      */
 
@@ -12,27 +12,32 @@ const caesar = function(plaintext, shift) {
          charCodeList.push(plaintext.charCodeAt(i));
      }
 
+     // Normalize the shift
+     let xShift = shift % 26;
+
      // Shift each character in the string, except special ones
      let shiftedCharList = [];
      for (let j = 0; j < plaintextLen; j++) {
         let resultCode = charCodeList[j];
          if (charCodeList[j] >= 65 && charCodeList[j] <= 90) {
-             resultCode = charCodeList[j] + shift;
+             // Character is confirmed as upper case letter
+             resultCode = charCodeList[j] + xShift;
              if (resultCode < 65) {
                  let wrapNum = 65 - resultCode;
-                 resultCode = 90 - wrapNum - 1;
+                 resultCode = 90 - wrapNum + 1;
              } else if (resultCode > 90) {
                  let wrapNum = resultCode - 90;
-                 resultCode = 65 + wrapNum -1;
+                 resultCode = 65 + wrapNum - 1;
              }
          } else if (charCodeList[j] >= 97 && charCodeList[j] <= 122) {
-             resultCode = charCodeList[j] + shift;
+             resultCode = charCodeList[j] + xShift;
+             // Character is confirmed as lower case letter
              if (resultCode < 97) {
                  let wrapNum = 97 - resultCode;
                  resultCode = 122 - wrapNum - 1;
              } else if (resultCode > 122) {
                  let wrapNum = resultCode - 122;
-                 resultCode = 97 + wrapNum + 1;
+                 resultCode = 97 + wrapNum - 1;
              } 
          }
          shiftedCharList.push(resultCode);
@@ -40,14 +45,13 @@ const caesar = function(plaintext, shift) {
 
      // Transfor code back to characters
      let transformedChars = [];
-     for (let k = 0; k <= plaintextLen; k++) {
+     for (let k = 0; k < plaintextLen; k++) {
          let resultChar = String.fromCharCode(shiftedCharList[k]);
          transformedChars.push(resultChar);
      }
 
      // Transform the array into a string
      let resultStr = transformedChars.join('');
-     console.log(resultStr);
 
      return resultStr;
 }
