c38ded8ca37bcc58ab3c473006de0a0d986fac89
Joan Advincula <jadvincula.cloud@protonmail.com>
2019-10-11 17:10:45 +0800

    Solve fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    let a = 1;
+    let b = 1;
+    
+    // Check if the number is positive
+    if (x < 1) {
+        return "OOPS"
+    }
 
+    // Transform to int if string is input
+    if (typeof(x) === "string") {
+        x = Number(x);
+    }
+
+    // Get the number
+    if (x == 1 || x == 2) {
+        return 1;
+    } else {
+        let f = 0;
+        for (let i = 3; i <= x; i++) {
+            f = a + b;
+            a = b;
+            b = f;
+        }
+
+        return f;
+    }
 }
 
 module.exports = fibonacci
