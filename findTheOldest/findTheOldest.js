aebdc42f305db8ceeb2421c70b5f218f8024cc43
Joan Advincula <jadvincula.cloud@protonmail.com>
2019-10-15 18:17:37 +0800

    Solve findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
 let findTheOldest = function(a) {
-    const
+    let oldest = a[0];
+    let oldestAge = 0;
+    
+    for (i in a) {
+        let age = 0;
+        if (a[i].hasOwnProperty("yearOfDeath")) {
+            age = a[i].yearOfDeath - a[i].yearOfBirth;
+        } else {
+            let d = new Date();
+            let y = d.getFullYear();
+            
+            age = y - a[i].yearOfBirth;
+        }
+
+        if (age > oldestAge) {
+            oldest = a[i];
+            oldestAge = age;
+        }
+    }
+
+    return oldest;
 }
 
 module.exports = findTheOldest
